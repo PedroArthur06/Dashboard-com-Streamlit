11efc6acffe86eed8775844bc7a6cc04280d5809
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-10 17:22:44 -0400

    Filtragem de datas efetuada

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -19,13 +19,24 @@ st.write("""
 O gráfico abaixa representa a evolução do preço das ações ao longo dos anos.
 """)
 
+st.sidebar.header("Filtros")
+
 # Filtragem das ações
-lista_acoes= st.multiselect("Escolha as ações para visualizar",dados.columns)
+lista_acoes= st.sidebar.multiselect("Escolha as ações para visualizar",dados.columns)
 if lista_acoes:
     dados= dados[lista_acoes]
     if len(lista_acoes) == 1:
         acao_unica = lista_acoes[0]
         dados = dados.rename(columns = {acao_unica: "Close"})
+
+# Filtro de datas
+data_inicial = dados.index.min().to_pydatetime()
+data_final = dados.index.max().to_pydatetime()
+intervalo_datas = st.sidebar.slider("Selecione o período", min_value= data_inicial, max_value= data_final, value= (data_inicial, data_final))
+
+dados = dados.loc[intervalo_datas[0]: intervalo_datas[1]]
+
+# Filtro de preço
 
 
 st.line_chart(dados)
