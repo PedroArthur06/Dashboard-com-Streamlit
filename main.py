6f857d5ad94d0c369ded122a5ae4b94270fd020b
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-10 16:29:05 -0400

    filtrei as ações pro usuario

--- main.py
+++ main.py
@@ -11,12 +11,21 @@ def carregarDados(empresas):
     cotacao_acao = cotacao_acao["Close"]
     return  cotacao_acao
 
-acoes= ["ITUB4.SA", "BBDC3.SA","PETR3.SA", "VALE3.SA", "ITSA4.SA"]
+acoes= ["ITUB4.SA", "BBDC3.SA","PETR3.SA", "VALE3.SA", "ITSA4.SA", "ABEV3.SA", "MGLU3.SA"]
 dados=carregarDados(acoes)
 
 st.write(""" 
 # App preço de ações
-O gráfico abaixa representa a evolução do preço das ações do Itaú(ITUB4.SA) e do Bradesco(BBDC3.SA) ao longo dos anos.
+O gráfico abaixa representa a evolução do preço das ações ao longo dos anos.
 """)
 
-st.line_chart(dados, use_container_width=True)
+
+lista_acoes= st.multiselect("Escolha as ações para visualizar",dados.columns)
+if lista_acoes:
+    dados= dados[lista_acoes]
+    if len(lista_acoes) == 1:
+        acao_unica = lista_acoes[0]
+        dados = dados.rename(columns = {acao_unica: "Close"})
+
+
+st.line_chart(dados)
