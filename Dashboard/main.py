09a955be3b906505ef84eb4803b28cf489644cca
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-11 18:20:42 -0400

    Código finalizado

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -1,75 +1,102 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
+from datetime import timedelta
 
+# Carrego os dadoos das empresas
 @st.cache_data
-def carregarDados(empresas):
+def carregar_dados(empresas):
     texto_tickers = " ".join(empresas)
-    dados = yf.Tickers(texto_tickers)
-    cotacao_acao = dados.history(period="1d", start="2010-01-01", end="2023-10-01")
-    print(cotacao_acao)
-    cotacao_acao = cotacao_acao["Close"]
-    return  cotacao_acao
+    dados_acao = yf.Tickers(texto_tickers)
+    cotacoes_acao = dados_acao.history(period="1d", start="2010-01-01", end="2024-07-01")
+    cotacoes_acao = cotacoes_acao["Close"]
+    return cotacoes_acao
 
+# carrego os tickers
 @st.cache_data
-def carregar_acoes():
+def carregar_tickers_acoes():
     base_tickers = pd.read_csv("IBOV.csv", sep=";")
     tickers = list(base_tickers["Código"])
     tickers = [item + ".SA" for item in tickers]
     return tickers
 
-acoes = carregar_acoes()
-dados=carregarDados(acoes)
+acoes = carregar_tickers_acoes()
+dados = carregar_dados(acoes)
 
-st.write(""" 
-# App preço de ações
-O gráfico abaixa representa a evolução do preço das ações ao longo dos anos.
+# Interface do streamlit
+st.write("""
+# App Preço de Ações
+O gráfico abaixo representa a evolução do preço das ações ao longo dos anos
 """)
 
+# Titulo da barra de filtros
 st.sidebar.header("Filtros")
 
-# Filtragem das ações
-lista_acoes= st.sidebar.multiselect("Escolha as ações para visualizar",dados.columns)
+# Filtro de ações
+lista_acoes = st.sidebar.multiselect("Escolha as ações para visualizar", dados.columns)
 if lista_acoes:
-    dados= dados[lista_acoes]
+    dados = dados[lista_acoes]
     if len(lista_acoes) == 1:
         acao_unica = lista_acoes[0]
-        dados = dados.rename(columns = {acao_unica: "Close"})
-
+        dados = dados.rename(columns={acao_unica: "Close"})
+        
 # Filtro de datas
 data_inicial = dados.index.min().to_pydatetime()
 data_final = dados.index.max().to_pydatetime()
-intervalo_datas = st.sidebar.slider("Selecione o período", min_value= data_inicial, max_value= data_final, value= (data_inicial, data_final))
+intervalo_data = st.sidebar.slider("Selecione o período", 
+                                   min_value=data_inicial, 
+                                   max_value=data_final,
+                                   value=(data_inicial, data_final),
+                                   step=timedelta(days=1))
 
-dados = dados.loc[intervalo_datas[0]: intervalo_datas[1]]
+dados = dados.loc[intervalo_data[0]:intervalo_data[1]]
 
+# Criar o grafico
 st.line_chart(dados)
 
-texto_perf= ""
 
-if len (lista_acoes) == 0:
+# Calculo de perfomance
+texto_performance_ativos = ""
+
+if len(lista_acoes)==0:
     lista_acoes = list(dados.columns)
+elif len(lista_acoes)==1:
+    dados = dados.rename(columns={"Close": acao_unica})
 
-elif len(lista_acoes) == 1:
-    dados = dados.rename(columns = {"Close": acao_unica})
 
-for acao in lista_acoes:
+carteira = [1000 for acao in lista_acoes]
+total_inicial_carteira = sum(carteira)
+
+for i, acao in enumerate(lista_acoes):
     performance_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
     performance_ativo = float(performance_ativo)
 
+    carteira[i] = carteira[i] * (1 + performance_ativo)
+
     if performance_ativo > 0:
-        texto_perf = texto_perf + f" \n{acao}: :green[{performance_ativo:.1%}] \n"
-
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :green[{performance_ativo:.1%}]"
     elif performance_ativo < 0:
-        texto_perf = texto_perf + f" \n{acao}: :red[{performance_ativo:.1%}] \n"
-
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: :red[{performance_ativo:.1%}]"
     else:
-        texto_perf = texto_perf + f" \n{acao}: {performance_ativo:.1%} \n"
+        texto_performance_ativos = texto_performance_ativos + f"  \n{acao}: {performance_ativo:.1%}"
+
+total_final_carteira = sum(carteira)
+performance_carteira = total_final_carteira / total_inicial_carteira - 1
+
+if performance_carteira > 0:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos: :green[{performance_carteira:.1%}]"
+elif performance_carteira < 0:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos: :red[{performance_carteira:.1%}]"
+else:
+    texto_performance_carteira = f"Performance da carteira com todos os ativos: {performance_carteira:.1%}"
 
 
-st.write(f""" 
-### Performace dos ativos
-Essa foi a performace de cada ativo no período selecionado:
-         
-{texto_perf}
+
+st.write(f"""
+### Performance dos Ativos
+Essa foi a perfomance de cada ativo no período selecionado:
+
+{texto_performance_ativos}
+
+{texto_performance_carteira}
 """)
