abd134c5c2dceba4ef2afba3cf361dda064feade
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-11 14:38:03 -0400

    90% completo

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -43,9 +43,6 @@ intervalo_datas = st.sidebar.slider("Selecione o período", min_value= data_inic
 
 dados = dados.loc[intervalo_datas[0]: intervalo_datas[1]]
 
-# Filtro de preço
-
-
 st.line_chart(dados)
 
 texto_perf= ""
@@ -53,10 +50,21 @@ texto_perf= ""
 if len (lista_acoes) == 0:
     lista_acoes = list(dados.columns)
 
+elif len(lista_acoes) == 1:
+    dados = dados.rename(columns = {"Close": acao_unica})
+
 for acao in lista_acoes:
     performance_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
     performance_ativo = float(performance_ativo)
-    texto_perf = texto_perf + f" \n{acao}: {performance_ativo:.1%} \n"
+
+    if performance_ativo > 0:
+        texto_perf = texto_perf + f" \n{acao}: :green[{performance_ativo:.1%}] \n"
+
+    elif performance_ativo < 0:
+        texto_perf = texto_perf + f" \n{acao}: :red[{performance_ativo:.1%}] \n"
+
+    else:
+        texto_perf = texto_perf + f" \n{acao}: {performance_ativo:.1%} \n"
 
 
 st.write(f""" 
